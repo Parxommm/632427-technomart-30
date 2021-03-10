40def35f2b65de3064907b19e4933c2a2c992288
Parxommm <evg.parkhomenko@gmail.com>
2021-03-10 18:54:23 +0300

    Декоративные элементы на внутренней странице
    Исправил замечания.
    JS пока еще в работе :)

--- js/script.js
+++ js/script.js
@@ -93,34 +93,3 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
-
-// modal--cart
-
-const cartPopup = document.querySelector(".modal--cart");
-const cartClose = cartPopup.querySelector(".modal__btn-close");
-
-cartClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (cartPopup.classList.contains("modal-show")) {
-      evt.preventDefault();
-      cartPopup.classList.remove("modal-show");
-    }
-  }
-});
-
-// .btn--buy
-
-const buyButtons = document.querySelectorAll(".btn--buy");
-
-for (var i = 0; i < buyButtons.length; i++) {
-  const buyButton = buyButtons[i];
-  buyButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    cartPopup.classList.add("modal-show");
-  });
-}
